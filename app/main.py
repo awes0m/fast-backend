f7867bcaf9de11aec6614e256129d89d2626e581
awes0m <somsubhra.pandit@gmail.com>
2022-05-24 17:57:59 +0530

    Added SQLAlchemy, and psycopg2 and database CRUD

--- app/main.py
+++ app/main.py
@@ -1,7 +1,16 @@
-from fastapi import FastAPI, HTTPException, Response, status
+import time
+from fastapi import Depends, FastAPI, HTTPException, Response, status
 from pydantic import BaseModel
-from typing import Optional
-from random import randint
+from app.utils.my_posts import my_posts
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from sqlalchemy.orm import Session
+
+from . import models
+from .database import engine, get_db
+
+
+models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
@@ -12,34 +21,54 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    likes = 0
 
 
-my_posts = [
-    {
-        "id": 1,
-        "title": "My first post",
-        "content": "This is my first post",
-        "published": True,
-        "rating": 1,
-    },
-    {
-        "id": 2,
-        "title": "My second post",
-        "content": "This is my second post",
-        "published": True,
-        "rating": 2,
-    },
-]
+# Database connection
+while True:
+    try:
+        conn = psycopg2.connect(
+            dbname="fast-backend",
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            cursor_factory=RealDictCursor,
+        )
+        cursor = conn.cursor()
+        print("Connected to the database")
+        break
+    except Exception as error:
+        print("I am unable to connect to the database")
+        print(f"ERROR:-  {error}")
+        time.sleep(2)
 
 
 def find_post(id):
+    """
+    The find_post function searches the list of posts
+    for a post with the given id.
+    If it finds that post, it returns that post's dictionary;
+    otherwise, it returns None.
+
+    :param id: Find the post with a specific id
+    :return: A dictionary
+    """
+
     for p in my_posts:
         if p["id"] == id:
             return p
 
 
 def find_index_post(id: int):
+    """
+    The find_index_post function finds the index of a post in my_posts that
+    has the same id as the parameter id.
+       If no such post exists, it returns None.
+
+    :param id:int: Specify the id of the post we want to find
+    :return: The index of the post in my_posts that has an id
+    equal to the value of id
+    """
     for i, p in enumerate(my_posts):
         if p["id"] == id:
             return i
@@ -52,30 +81,70 @@ async def root():
 
 
 @app.get("/posts")
-def get_posts():
+def get_posts(db: Session = Depends(get_db)):
     """
-    The get_posts function returns a list of all the blog posts.
-    :return: A list of dictionaries
+    The get_posts function returns all the posts in the database.
+
+    :param db:Session=Depends(get_db): Pass the database session
+    to the function
+    :return: A list of all the posts in the database
     """
-    return {"data": my_posts}
+
+    # RAW_SQL
+    # cursor.execute("""SELECT * FROM posts""")
+    # posts = cursor.fetchall()
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
-def create_posts(post: Post):
-    post_dict = dict(post)
-    post_dict["id"] = randint(1, 10000)
-    my_posts.append(post_dict)
-    return {"data": post}
+def create_posts(post: Post, db: Session = Depends(get_db)):
+    """
+    The create_posts function creates a new post in the database.
+    It takes as input a Post object and returns the created post.
+
+    :param post:Post: Pass in the data from the api request
+    :param db:Session=Depends(get_db): Inject the database session dependency
+    into the function
+    :return: A post object
+    """
+
+    #  Works BUT Vulnerable to SQL Injection 👀
+    # cursor.execute(
+    #     """INSERT INTO posts (title, content, published) VALUES
+    #     (post.title, post.content, post.published, post.likes)"""
+    # )
+
+    # RAW SQL
+    # cursor.execute(
+    #     """INSERT INTO posts (title, content, published) /
+    #     VALUES (%s, %s, %s) RETURNING *""",
+    #     (post.title, post.content, post.published),
+    # )
+    # conn.commit()
+
+    new_post = models.Post(**post.dict())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return {"data": new_post}
 
 
 @app.get("/posts/{post_id}")
-def get_post(post_id: int, response: Response):
-    """The get_post function returns a post with the given id.
-    :param post_id: Specify the id [int] of the post that we want to retrieve
-    :param response:Response: Return the response object
-    :return: The post with the id passed in"""
+def get_post(post_id: int, db: Session = Depends(get_db)):
+    """
+    The get_post function returns a post with the given id.
 
-    post = find_post(post_id)
+    :param post_id:int: Capture the id of the post we want to retrieve
+    :param db:Session=Depends(get_db): Inject the database session 
+    into the function
+    :return: A tuple with one element, the post with the given id
+    """
+
+    # RAW SQL
+    # cursor.execute("""SELECT * FROM posts WHERE id = %s""", (str(post_id),))
+    # post = (cursor.fetchone(),)
+    post = db.query(models.Post).filter(models.Post.post_id == post_id).first()
     if not post:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail="Post not found"
@@ -84,45 +153,68 @@ def get_post(post_id: int, response: Response):
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(post_id: int):
-    """The delete_post function removes a post from the list of posts.
-    Parameters:
-        post_id (int): The id of the post to be deleted."""
+def delete_post(post_id: int, db: Session = Depends(get_db)):
+    """
+    The delete_post function will delete a post from the database.
+    It will return the deleted post object on success
+    or an error message if it fails.
 
-    index = find_index_post(post_id)
-    if index is None:
+    :param post_id:int: Specify the id of the post to be deleted
+    :param db:Session=Depends(get_db): Pass the database session to function
+    :return: A dictionary with the id of the deleted post
+    :doc-author: Trelent
+    """
+
+    # cursor.execute(
+    #     """DELETE FROM posts WHERE/
+    #                id = %s RETURNING *""",
+    #     (str(post_id),),
+    # )
+    # deleted_post = cursor.fetchone()
+    # conn.commit()
+    deleted_post = db.query(models.Post).filter(models.Post.post_id == post_id)
+    if deleted_post.first() is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id-{post_id} not found",
         )
-    my_posts.pop(index)
+    deleted_post.delete(synchronize_session=False)
+    db.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{post_id}")
-def update_post(post_id: int, post: Post):
-    """
-    The update_post function updates a post in the my_posts list.
-    It takes two arguments:
-        -post_id: an integer representing the id of the post to be updated, and
-        -post: a Post object containing all of the information needed to update that particular post.
+def update_post(post_id: int, post: Post, db: Session = Depends(get_db)):
 
-       It returns None.
+    # RAW SQL
+    # cursor.execute(
+    #     """UPDATE posts SET title = %s, content = %s, published = %s /
+    #     WHERE id = %s RETURNING *""",
+    #     (
+    #         post.title,
+    #         post.content,
+    #         post.published,
+    #         str(post_id),
+    #     ),
+    # )
+    # updated_post = cursor.fetchone()
+    # conn.commit()
 
-    :param post_id:int: Find the index of the post in my_posts
-    :param post:Post: Create a new post
-    :return: The new post with the updated information
-    """
-
-    index = find_index_post(post_id)
-    if index is None:
+    post_query = db.query(models.Post).filter(models.Post.post_id == post_id)
+    updated_post = post_query.first()
+    if updated_post is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Post with id-{post_id} not found",
         )
-    post_dict = post.dict()
-    print(post_dict)
-    post_dict["id"] = post_id
-    my_posts[index] = post_dict
 
-    return {"data": post_dict}
+    post_query.update(post.dict(), synchronize_session=False)
+    db.commit()
+
+    return {"data": post_query.first()}
+
+
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
---
+++ app/utils/my_posts.py
@@ -0,0 +1,16 @@
+my_posts = [
+    {
+        "id": 1,
+        "title": "My first post",
+        "content": "This is my first post",
+        "published": True,
+        "likes": 0,
+    },
+    {
+        "id": 2,
+        "title": "My second post",
+        "content": "This is my second post",
+        "published": True,
+        "likes": 5,
+    },
+]
